4e3745e597cc8d50015df611852bf838cc5adf32
josemariamv <jmoralesvazquez@educa.madrid.org>
2025-10-13 14:43:42 +0200

    Clases optativa Fundamentos de Programación

---
+++ conjuntos.py
@@ -0,0 +1,3 @@
+# Los conjuntos en python están formados por elementos "sin orden"
+# y por tanto no se pueden referenciar # sus elementos por la posición que ocupan.
+# Tampoco pueden tener elementos duplicados
