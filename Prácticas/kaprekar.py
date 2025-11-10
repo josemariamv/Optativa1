5d577537a84c5946e8bf59a07f49df0dcce8e14f
josemariamv <jmoralesvazquez@educa.madrid.org>
2025-11-10 10:18:22 +0100

    constante de kaprekar comentado

--- Prácticas/kaprekar.py
+++ Prácticas/kaprekar.py
@@ -1,10 +1,14 @@
 entrada = input("Introduce un número de cuatro cifras que no tenga todas sus cifras iguales: ")
+# No tenemos constantes, pero las simbolizamos con variables en mayúsculas
 KAPREKAR = 6174
+# Primero hacemos comprobaciones: que no sea un número entero positivo
 if entrada.isdigit() == False:
     print("No has introducido un número entero")
+# que no tenga cuatro cifras
 elif len(entrada)!=4:
     print("Tú número no tiene exactamente 4 cifras")
 else:
+    # que no sean todas iguales
     hayDiferencias = False;
     for i in range(1,4):
         if entrada[0]!=entrada[i]:
@@ -14,19 +18,24 @@ else:
     else:
         numero = int(entrada)
         texto = entrada
+        # o que el número ya sea la constante de kaprekar
         if numero == KAPREKAR:
             print("El número que has introducido ya es la constante de kaprekar")
         else:
+            # en caso contrario empezamos el proceso descrito en el enunciado
             contador = 0
             while numero!=KAPREKAR:
                 contador += 1
+                # En listaAscendente guardo los cuatro dígitos del número ordenados de menor a mayor
                 listaAscendente = list(texto)
                 listaAscendente.sort();
                 txtAscendente = ""
                 txtDescendente = ""
+                # Ahora recorro la lista y compongo los dos números en formato string
                 for i in range(0,4):
                     txtAscendente = txtAscendente + listaAscendente[i];
                     txtDescendente = listaAscendente[i] + txtDescendente;
+                # los resto y obtengo la diferencia
                 numero = int(txtDescendente) - int(txtAscendente)
                 texto = str(numero)
                 # Si el número resultante tiene menos de cuatro cifras completo con ceros a la izquierda
