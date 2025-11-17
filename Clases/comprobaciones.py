36d42a55ef936c0f84c39a500f3907eeb6344fd0
josemariamv <jmoralesvazquez@educa.madrid.org>
2025-11-17 09:18:43 +0100

    mas funciones de comprobación. Correcciones en funciones. Creación de condicionalif (por terminar)

--- Clases/comprobaciones.py
+++ Clases/comprobaciones.py
@@ -14,3 +14,25 @@ if cadena.isalnum():
     print("Son letras y números")
 
 #Existen además isdecimal, isnumeric, isspace e isprintable, etc.
+
+if isinstance(5,int):
+    print("Es un entero")
+else:
+    print("No es un entero")
+
+if not isinstance(5.5,float):
+    print("No es un número con decimales")
+else:
+    print("Es un número con decimales")
+
+print(type("HOLA"))
+
+if isinstance("hola",str):
+    print("Es un texto")
+else:
+    print("No es un texto")
+
+if isinstance([1,2,"Pepe"],list):
+    print("Es una lista")
+else:
+    print("No es una lista")
