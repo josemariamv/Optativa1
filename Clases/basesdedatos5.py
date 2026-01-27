93b2d4de1604abafe4e822963bcd4ba6beddddf9
josemariamv <jmoralesvazquez@educa.madrid.org>
2026-01-27 20:27:55 +0100

    añadidos en bases de datos

--- Clases/basesdedatos5.py
+++ Clases/basesdedatos5.py
@@ -1,19 +1,20 @@
-
 import mysql.connector
 
 try:
     connect = mysql.connector.connect(user='josemaria', password='abc123', host='localhost', database='pokemondb')
     cursor = connect.cursor()
 
-    sql = ("SELECT nombre, peso, altura from pokemon where peso > 250")
+    sql = "SELECT nombre, peso, altura FROM pokemon WHERE peso > 200"
     cursor.execute(sql)
 
     for fila in cursor:
         print(fila)
 
-    sql = ("UPDATE pokemon SET peso=350 WHERE peso > 250")
+    sql = "UPDATE pokemon SET peso=350 WHERE peso > 250"
     cursor.execute(sql)
     # el metodo rowcount nos devuelve, en updates y deletes, el número de filas involucradas
+    # en select no lo hace con 'nonbuffered' cursors que son los que estamos viendo
+    # existe la opción de buffered cursors, pero no los vamos a ver
     print("La instrucción involucra a", cursor.rowcount, "filas de la base de datos")
     # si no ejecutamos un commit no se guardan los cambios
     #connect.commit()
@@ -22,55 +23,63 @@ try:
 except mysql.connector.Error as err:
     print(err)
 
-'''
-insert_stmt = (
-  "INSERT INTO employees (emp_no, first_name, last_name, hire_date) "
-  "VALUES (%s, %s, %s, %s)"
-)
-data = (2, 'Jane', 'Doe', datetime.date(2012, 3, 23))
-cursor.execute(insert_stmt, data)
+try:
+    connect = mysql.connector.connect(user='josemaria', password='abc123', host='localhost', database='pokemondb')
+    cursor = connect.cursor()
 
-select_stmt = "SELECT * FROM employees WHERE emp_no = %(emp_no)s"
-cursor.execute(select_stmt, { 'emp_no': 2 })
+    # Podemos parametrizar el select igual que en JDBC
+    sql = "SELECT nombre, peso, altura FROM pokemon WHERE peso > %s AND nombre = %s"
+    datos = (250,'Snorlax')
+    cursor.execute(sql, datos)
 
-data = [
-  ('Jane', date(2005, 2, 12)),
-  ('Joe', date(2006, 5, 23)),
-  ('John', date(2010, 10, 3)),
-]
-stmt = "INSERT INTO employees (first_name, hire_date) VALUES (%s, %s)"
-cursor.executemany(stmt, data)
+    for fila in cursor:
+        print(fila)
 
->>> cursor.execute("SELECT * FROM employees ORDER BY emp_no")
->>> head_rows = cursor.fetchmany(size=2)
->>> remaining_rows = cursor.fetchall()
+    # Y lanzar múltiples querys (no válido con SELECT)
+    sql = "INSERT INTO pokemon (numero_pokedex, nombre, peso, altura) VALUES (%s, %s, %s, %s)"
+    datos = [(200, 'Pepe Potamo', 50, 30), (201, 'Inés Perado', 33, 600), (202,'Benito Camelas', 412, 30)]
+    cursor.executemany(sql, datos)
+    print("La instrucción involucra a", cursor.rowcount, "filas de la base de datos")
 
-# Using a while loop
-cursor.execute("SELECT * FROM employees")
-row = cursor.fetchone()
-while row is not None:
-  print(row)
-  row = cursor.fetchone()
+    cursor.execute("SELECT * FROM pokemon WHERE numero_pokedex >=200")
+    for fila in cursor:
+        print(fila)
+    # si no ejecutamos un commit no se guardan los cambios
+    #connect.commit()
+    cursor.close()
+    connect.close()
+except mysql.connector.Error as err:
+    print(err)
 
-# Using the cursor as iterator
-cursor.execute("SELECT * FROM employees")
-for row in cursor:
-  print(row)
+try:
+    connect = mysql.connector.connect(user='josemaria', password='abc123', host='localhost', database='pokemondb')
+    cursor = connect.cursor()
+    # Activamos la posibilidad de ver los warnings
+    cursor.get_warnings = True
 
->>> cnx.get_warnings = True
->>> cursor.execute("SELECT 'a'+1")
->>> cursor.fetchall()
-[(1.0,)]
->>> print(cursor.warnings)
-[(u'Warning', 1292, u"Truncated incorrect DOUBLE value: 'a'")]
+    sql = "SELECT numero_pokedex, nombre FROM pokemon where numero_pokedex <=10"
+    cursor.execute(sql)
+    # Con fetchmany podemos indicar cuantas filas leer
+    dosFilas = cursor.fetchmany(size=2)
+    # y luego leer las restantes
+    restantes = cursor.fetchall()
+    print(dosFilas)
+    print(restantes)
 
-cnx = mysql.connector.connect(user='scott', database='test')
-cursor = cnx.cursor()
-operation = 'SELECT 1; UPDATE t1 SET c1 = 2; SELECT 2'
-for result in cursor.execute(operation):
-  if result.with_rows:
-    result.fetchall()
-  else:
-    print("Number of affected rows: {}".format(result.rowcount))
+    # volvemos a ejecutar la query
+    cursor.execute(sql)
+    # y ahora vamos a ver otro método de leer una a una las filas de la query
+    fila = cursor.fetchone()
+    # sería igual que fila =cursor.fetchmany(size=1)
+    # y luego iteramos con un bucle
+    while fila is not None:
+        print(fila)
+        fila = cursor.fetchone()
 
-'''
+    # si hubiera warnings los veríamos aquí
+    print(cursor._warnings)
+
+    cursor.close()
+    connect.close()
+except mysql.connector.Error as err:
+    print(err)
