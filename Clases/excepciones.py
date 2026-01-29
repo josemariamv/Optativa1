db487054c338831704220f6220af1e29fcec3724
josemariamv <jmoralesvazquez@educa.madrid.org>
2026-01-29 08:44:18 +0100

    pequeños cambios en excepciones

--- Clases/excepciones.py
+++ Clases/excepciones.py
@@ -22,6 +22,7 @@ except ZeroDivisionError:
 except:
     print("Otra excepción que no identifico")
 
+# Extras
 # Con raise podemos lanzar nuestras propias excepciones
 n = int(input("Introduce un entero positivo: "))
 if n < 0:
@@ -32,11 +33,11 @@ raise ZeroDivisionError("No has dividido por cero en realidad, pero ahí va eso"
 
 # El metodo assert me permite lanzar una excepción si lo que se evalúa como argumento es falso
 # Las dos líneas siguientes son equivalentes
-assert 1==3
-assert(1==3)
+# assert 1==3
+# assert(1==3)
 # El segundo parámetro, opcional, permite personalizar el mensaje de error
-assert (1==3, "1 no es igual a 3")
-assert 1==3, "1 no es igual a 3"
+# assert (1==3, "1 no es igual a 3")
+# assert 1==3, "1 no es igual a 3"
 
 
 
