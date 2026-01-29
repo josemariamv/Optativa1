039569bd83e55f3b2d5b825b827bd9a756fb49ce
josemariamv <jmoralesvazquez@educa.madrid.org>
2026-01-29 08:31:30 +0100

    ultimos añadidos en bases de datos

--- Clases/basesdedatos5.py
+++ Clases/basesdedatos5.py
@@ -1,5 +1,9 @@
 import mysql.connector
 
+# Algunos ejemplos extras de acceso a bases de datos desde python
+# Usamos la base de datos pokemondb
+# Por claridad, cada ejemplo está en un bloque try/excep diferente
+
 try:
     connect = mysql.connector.connect(user='josemaria', password='abc123', host='localhost', database='pokemondb')
     cursor = connect.cursor()
@@ -14,9 +18,10 @@ try:
     cursor.execute(sql)
     # el metodo rowcount nos devuelve, en updates y deletes, el número de filas involucradas
     # en select no lo hace con 'nonbuffered' cursors que son los que estamos viendo
-    # existe la opción de buffered cursors, pero no los vamos a ver
+    # existe la opción de usar buffered cursors, pero no los vamos a ver
     print("La instrucción involucra a", cursor.rowcount, "filas de la base de datos")
-    # si no ejecutamos un commit no se guardan los cambios
+    # recuerda que si no ejecutamos un commit no se guardan los cambios
+    # en este ejemplo está comentado y no se guardan
     #connect.commit()
     cursor.close()
     connect.close()
@@ -27,26 +32,29 @@ try:
     connect = mysql.connector.connect(user='josemaria', password='abc123', host='localhost', database='pokemondb')
     cursor = connect.cursor()
 
-    # Podemos parametrizar el select igual que en JDBC
+    # Podemos parametrizar el select igual que con los statement de JDBC
     sql = "SELECT nombre, peso, altura FROM pokemon WHERE peso > %s AND nombre = %s"
     datos = (250,'Snorlax')
+    # cada elemento de la tupla datos se sustituye en orden por el %s del query
     cursor.execute(sql, datos)
 
     for fila in cursor:
         print(fila)
 
-    # Y lanzar múltiples querys (no válido con SELECT)
+    # También podemos lanzar múltiples querys (no válido con SELECT)
     sql = "INSERT INTO pokemon (numero_pokedex, nombre, peso, altura) VALUES (%s, %s, %s, %s)"
     datos = [(200, 'Pepe Potamo', 50, 30), (201, 'Inés Perado', 33, 600), (202,'Benito Camelas', 412, 30)]
+    # En este caso, cada tupla se corresponde con un query diferente
+    # Fíjate que usamos executemany y no execute
     cursor.executemany(sql, datos)
     print("La instrucción involucra a", cursor.rowcount, "filas de la base de datos")
 
     cursor.execute("SELECT * FROM pokemon WHERE numero_pokedex >=200")
     for fila in cursor:
         print(fila)
-    # si no ejecutamos un commit no se guardan los cambios
-    #connect.commit()
 
+    # tampoco guardamos los cambios, que no queremos cargarnos una base de datos tan bonita :-)
+    #connect.commit()
     cursor.close()
     connect.close()
 except mysql.connector.Error as err:
@@ -56,13 +64,14 @@ try:
     connect = mysql.connector.connect(user='josemaria', password='abc123', host='localhost', database='pokemondb')
     cursor = connect.cursor()
     # Activamos la posibilidad de ver los warnings
+    # En este ejemplo no provocamos ninguno, así que al final no veremos nada...
     cursor.get_warnings = True
 
     sql = "SELECT numero_pokedex, nombre FROM pokemon where numero_pokedex <=10"
     cursor.execute(sql)
     # Con fetchmany podemos indicar cuantas filas leer
     dosFilas = cursor.fetchmany(size=2)
-    # y luego leer las restantes
+    # y luego leer las restantes a partir de donde nos hemos quedado
     restantes = cursor.fetchall()
     print(dosFilas)
     print(restantes)
@@ -71,7 +80,7 @@ try:
     cursor.execute(sql)
     # y ahora vamos a ver otro método de leer una a una las filas de la query
     fila = cursor.fetchone()
-    # sería igual que fila =cursor.fetchmany(size=1)
+    # fetchone sería igual que fila =cursor.fetchmany(size=1)
     # y luego iteramos con un bucle
     while fila is not None:
         print(fila)
